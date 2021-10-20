1c17fb3e9d6893792e3f9456416daef26f20ae83
aaxelr <antonio.axelsson@gmail.com>
2021-10-20 23:41:34 +0200

    setup cors

--- server/app.js
+++ server/app.js
@@ -1,6 +1,15 @@
+require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
+
+const corsOrigin = process.env.CLIENT_URL || 'http://localhost:3000';
+
+app.use(express.json());
+app.use(cors({
+  origin: corsOrigin,
+}));
 
 app.get('/', (req, res) => {
   res.send('<h1>TJABA</h1>');
