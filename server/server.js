d2f0bc51ef86f64f9e0e0ec816e58fb446865ca5
aaxelr <antonio.axelsson@gmail.com>
2021-10-20 21:23:39 +0200

    eslint disable next line no console

--- server/server.js
+++ server/server.js
@@ -2,4 +2,7 @@ const app = require('./app');
 
 const port = process.env.PORT || 5000;
 
-app.listen(5000, () => console.log(`listening on port: ${port}...`));
+app.listen(5000, () => {
+  // eslint-disable-next-line no-console
+  console.log(`listening on port: ${port}...`);
+});
