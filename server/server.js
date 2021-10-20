4afd1d57c7e73b70114aae75cb962003f0944983
aaxelr <antonio.axelsson@gmail.com>
2021-10-20 21:57:56 +0200

    setup db connection

--- server/server.js
+++ server/server.js
@@ -1,8 +1,20 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
 const app = require('./app');
 
 const port = process.env.PORT || 5000;
+const db = process.env.DB_URI;
 
-app.listen(5000, () => {
-  // eslint-disable-next-line no-console
-  console.log(`listening on port: ${port}...`);
-});
+mongoose.connect(db)
+  .then(() => {
+    app.listen(5000, () => {
+      // eslint-disable-next-line no-console
+      console.log('Connected to database');
+      // eslint-disable-next-line no-console
+      console.log(`listening on port: ${port}...`);
+    });
+  })
+  .catch((error) => {
+    // eslint-disable-next-line no-console
+    console.error('Failed to connect to database.', error);
+  });
