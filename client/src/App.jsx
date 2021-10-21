1e108a2f76814849349f9ed4f3e436231d84dd57
aaxelr <antonio.axelsson@gmail.com>
2021-10-22 01:18:05 +0200

    install material-ui, roboto font

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import Typography from '@mui/material/Typography';
 
 function App() {
   return (
     <div>
-      <h1>Hello</h1>
+      <Typography variant="h1">Hello</Typography>
     </div>
   );
 }
