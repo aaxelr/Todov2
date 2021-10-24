49bc16113623243f7d80a71da8b3cbb2607670ee
aaxelr <antonio.axelsson@gmail.com>
2021-10-24 22:58:09 +0200

    add toggle function

--- client/src/components/Todo.jsx
+++ client/src/components/Todo.jsx
@@ -1,14 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import Typography from '@mui/material/Typography';
+import { Box, Button } from '@mui/material';
+import MuiContainer from './UI/MuiContainer';
 
 function Todo({ todo }) {
+  const [showTodo, setShowTodo] = useState(false);
+
+  const handleToggleTodo = () => {
+    setShowTodo((prevState) => !prevState);
+  };
+
+  // TODO Fix styling
   return (
-    <div>
-      <Typography component="h2" variant="h4">
-        {todo.title}
-      </Typography>
-    </div>
+    <MuiContainer>
+      <Box
+        sx={{
+          maxWidth: '60%',
+          display: 'flex',
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+        }}
+      >
+        <Typography component="h2" variant="h4">
+          {todo.title}
+        </Typography>
+        <Button
+          onClick={handleToggleTodo}
+          color="primary"
+          variant={showTodo ? 'outlined' : 'contained'}
+        >
+          {showTodo ? 'Hide' : 'Show'}
+        </Button>
+      </Box>
+      {showTodo && (
+        <Typography component="p" variant="body1">
+          {todo.body}
+        </Typography>
+      )}
+    </MuiContainer>
   );
 }
 
