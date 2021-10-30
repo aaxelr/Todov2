8fb27cd5022137176d7b881498a891ff146e3c64
aaxelr <antonio.axelsson@gmail.com>
2021-10-31 00:10:41 +0200

    add MongoStore, session

--- server/app.js
+++ server/app.js
@@ -1,14 +1,28 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
 
 const app = express();
 
 const corsOrigin = process.env.CLIENT_URL || 'http://localhost:3000';
 
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors({
   origin: corsOrigin,
+}));
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  store: MongoStore.create({
+    mongoUrl: process.env.DB_URI,
+  }),
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    maxAge: 1000 * 60 * 60 * 24 * 7,
+  },
 }));
 
 app.get('/', (req, res) => {
