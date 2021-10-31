9c0c86adb5b1df27abb6d8326dabc4340bf1b786
aaxelr <antonio.axelsson@gmail.com>
2021-10-31 23:52:12 +0100

    setup redux

---
+++ client/src/app/auth.js
@@ -0,0 +1,21 @@
+/* eslint-disable no-param-reassign */
+import { createSlice } from '@reduxjs/toolkit';
+
+export const authSlice = createSlice({
+  name: 'auth',
+  initialState: {
+    isAuthenticated: false,
+    user: null,
+  },
+  reducers: {
+    setIsAuthenticated: (state, action) => {
+      state.isAuthenticated = action.payload;
+    },
+    setUser: (state, action) => {
+      state.user = action.payload;
+    },
+  },
+});
+
+export const { setIsAuthenticated, setUser } = authSlice.actions;
+export default authSlice.reducer;
