9d21b25e394131a4f90b2cd1ba2d57edf19db02f
aaxelr <antonio.axelsson@gmail.com>
2021-11-06 00:21:43 +0100

    pass onDeleteTodo to Todo component

--- client/src/components/Todos.jsx
+++ client/src/components/Todos.jsx
@@ -3,16 +3,21 @@ import PropTypes from 'prop-types';
 import Todo from './Todo';
 import MuiContainer from './UI/MuiContainer';
 
-function Todos({ todos }) {
+function Todos({ todos, onDeleteTodo }) {
   return (
     <MuiContainer>
-      {todos.map((todo) => <Todo todo={todo} key={todo.uuid} />)}
+      {!todos
+      && <p>loading...</p>}
+      {todos && console.log(todos)}
+      {todos.length > 0
+      && todos.map((todo) => <Todo todo={todo} key={todo.uuid} onDeleteTodo={onDeleteTodo} />)}
     </MuiContainer>
   );
 }
 
 Todos.propTypes = {
   todos: PropTypes.arrayOf(PropTypes.object).isRequired,
+  onDeleteTodo: PropTypes.func.isRequired,
 };
 
 export default Todos;
