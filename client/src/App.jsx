82c89fcbbf5aeb42fdaf73cd8e4f3d3f98564ab9
aaxelr <antonio.axelsson@gmail.com>
2021-11-06 00:05:02 +0100

    add switch, routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,30 @@
 import React from 'react';
-import Typography from '@mui/material/Typography';
-import NewTodo from './components/NewTodo';
+import { Route, Switch } from 'react-router-dom';
+import { Button } from '@mui/material/';
+import Header from './components/Header';
+import MuiContainer from './components/UI/MuiContainer';
+import DashBoardPage from './pages/DashboardPage';
+import LoginErrorPage from './pages/LoginErrorPage';
+import LoginSuccessPage from './pages/LoginSuccessPage';
 
 function App() {
   return (
-    <div>
-      {/* TODO material ui container */}
-      {/* TODO navbar */}
-      {/* TODO login */}
-      <Typography variant="h3" component="h1">Todo or not Todo</Typography>
-      <NewTodo />
-      {/* TODO render todos */}
-    </div>
+    <MuiContainer>
+      <Header />
+      <Switch>
+        <Route exact path="/">
+          <Button
+            href="http://localhost:5000/auth/google"
+            variant="contained"
+          >
+            Sign in with Goggles 😎
+          </Button>
+        </Route>
+        <Route exact path="/dashboard" component={DashBoardPage} />
+        <Route exact path="/login/success" component={LoginSuccessPage} />
+        <Route exact path="/login/error" component={LoginErrorPage} />
+      </Switch>
+    </MuiContainer>
   );
 }
 
