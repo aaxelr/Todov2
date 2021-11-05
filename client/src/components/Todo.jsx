e47a772b518d282a8c6be1cdddfd32e2ae71d226
aaxelr <antonio.axelsson@gmail.com>
2021-11-06 00:22:33 +0100

    add ReactMarkdown, add edit & delete btns

--- client/src/components/Todo.jsx
+++ client/src/components/Todo.jsx
@@ -1,49 +1,100 @@
 import React, { useState } from 'react';
+import ReactMarkdown from 'react-markdown';
 import PropTypes from 'prop-types';
-import Typography from '@mui/material/Typography';
-import { Box, Button } from '@mui/material';
+import {
+  Box,
+  Button,
+  ButtonGroup,
+  Typography,
+} from '@mui/material';
+import EditIcon from '@mui/icons-material/Edit';
+import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
 import MuiContainer from './UI/MuiContainer';
 
-function Todo({ todo }) {
+function Todo({ todo, onDeleteTodo }) {
   const [showTodo, setShowTodo] = useState(false);
 
   const handleToggleTodo = () => {
     setShowTodo((prevState) => !prevState);
   };
 
+  const handleDeleteTodo = () => {
+    onDeleteTodo(todo.uuid);
+  };
+
   // TODO Fix styling
   return (
     <MuiContainer>
-      <Box
-        sx={{
-          maxWidth: '60%',
-          display: 'flex',
-          flexDirection: 'row',
-          justifyContent: 'space-between',
-        }}
+      <Box sx={{
+        background: '#fff',
+        border: '1px solid #111',
+        padding: '1rem',
+      }}
       >
-        <Typography component="h2" variant="h4">
-          {todo.title}
-        </Typography>
-        <Button
-          onClick={handleToggleTodo}
-          color="primary"
-          variant={showTodo ? 'outlined' : 'contained'}
+        <Box
+          sx={{
+            display: 'flex',
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+          }}
         >
-          {showTodo ? 'Hide' : 'Show'}
-        </Button>
+          <Typography component="h2" variant="h4">
+            {todo.title}
+          </Typography>
+          <Button
+            onClick={handleToggleTodo}
+            color="primary"
+            variant={showTodo ? 'outlined' : 'contained'}
+          >
+            {showTodo ? 'Hide' : 'Show'}
+          </Button>
+        </Box>
+        {showTodo && (
+        <div>
+          <ReactMarkdown>
+            {todo.body}
+          </ReactMarkdown>
+          <ButtonGroup
+            fullWidth
+            sx={{
+              margin: '.5rem',
+            }}
+          >
+            <Button
+              variant="outlined"
+              startIcon={<DeleteForeverIcon />}
+              color="error"
+              onClick={handleDeleteTodo}
+            >
+              Delete
+            </Button>
+            <Button
+              variant="contained"
+              startIcon={<EditIcon />}
+              color="primary"
+              type="submit"
+            >
+              Edit Todo
+            </Button>
+          </ButtonGroup>
+        </div>
+        )}
       </Box>
-      {showTodo && (
-        <Typography component="p" variant="body1">
-          {todo.body}
-        </Typography>
-      )}
     </MuiContainer>
   );
 }
 
 Todo.propTypes = {
-  todo: PropTypes.objectOf(PropTypes.string).isRequired,
+  todo: PropTypes.shape({
+    __v: PropTypes.number,
+    _id: PropTypes.string,
+    author: PropTypes.string,
+    body: PropTypes.string,
+    title: PropTypes.string,
+    updated: PropTypes.string,
+    uuid: PropTypes.string,
+  }).isRequired,
+  onDeleteTodo: PropTypes.func.isRequired,
 };
 
 export default Todo;
