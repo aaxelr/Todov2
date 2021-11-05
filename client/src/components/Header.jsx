1cfafdf2fe494f06da4201fa490c37901893a37d
aaxelr <antonio.axelsson@gmail.com>
2021-11-06 00:20:38 +0100

    basic header component

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Typography from '@mui/material/Typography';
+
+function Header() {
+  return (
+    <Typography component="h1" variant="h2" sx={{ padding: '1rem' }}>Todo or not Todo</Typography>
+  );
+}
+
+export default Header;
