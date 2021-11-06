8f7c1704239290089977a275fed36260be1f958e
aaxelr <antonio.axelsson@gmail.com>
2021-11-06 14:49:48 +0100

    create logout button w logout functionality

---
+++ client/src/components/LogOutButton.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { Button } from '@mui/material';
+import ExitToAppIcon from '@mui/icons-material/ExitToApp';
+import axios from 'axios';
+import { useHistory } from 'react-router-dom';
+import { setIsAuthenticated, setUser } from '../app/auth';
+
+function LogOutButton() {
+  const history = useHistory();
+  const dispatch = useDispatch();
+  const [errors, setErrors] = useState([]);
+  const API_URL = process.env.REACT_API_URL || 'http://localhost:5000';
+
+  const logoutUser = async () => {
+    const response = await axios.get(`${API_URL}/auth/logout`, { withCredentials: true })
+      .catch((error) => {
+        setErrors((prevErrors) => [...prevErrors, error]);
+      });
+    console.log(response);
+    if (response && response.status === 205) {
+      dispatch(setIsAuthenticated(false));
+      dispatch(setUser(null));
+      history.push('/');
+    }
+  };
+
+  if (errors.length > 0) {
+    return errors.map((error) => <p>{error.message}</p>);
+  }
+
+  return (
+    <Button
+      variant="outlined"
+      startIcon={<ExitToAppIcon />}
+      onClick={logoutUser}
+    >
+      Log Out
+    </Button>
+  );
+}
+
+export default LogOutButton;
