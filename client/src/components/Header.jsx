36a9d1d1afbea79c9207e8c2b41e5dbd9ab9f9f9
aaxelr <antonio.axelsson@gmail.com>
2021-11-06 14:49:30 +0100

    update header, add logout button

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,9 +1,37 @@
 import React from 'react';
-import Typography from '@mui/material/Typography';
+import { useSelector } from 'react-redux';
+import { Container, Typography } from '@mui/material';
+import MuiContainer from './UI/MuiContainer';
+import LogOutButton from './LogOutButton';
 
 function Header() {
+  const { user } = useSelector((state) => state.auth);
+
   return (
-    <Typography component="h1" variant="h2" sx={{ padding: '1rem' }}>Todo or not Todo</Typography>
+    <MuiContainer>
+      <Typography
+        component="h1"
+        variant="h2"
+        sx={{ padding: '1rem' }}
+      >
+        Todo or not Todo
+      </Typography>
+      {user && (
+        <Container sx={{
+          display: 'flex',
+          justifyContent: 'space-between',
+          alignItems: 'baseline',
+        }}
+        >
+          <Typography component="p" variant="body2">
+            Logged in as
+            {' '}
+            {user.fullName}
+          </Typography>
+          <LogOutButton />
+        </Container>
+      )}
+    </MuiContainer>
   );
 }
 
