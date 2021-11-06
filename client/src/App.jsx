309f7c4bab23bae47c5747f003f8cf4be35215d1
aaxelr <antonio.axelsson@gmail.com>
2021-11-06 14:49:10 +0100

    remove header from outside switch

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,25 +1,16 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
-import { Button } from '@mui/material/';
-import Header from './components/Header';
 import MuiContainer from './components/UI/MuiContainer';
 import DashBoardPage from './pages/DashboardPage';
 import LoginErrorPage from './pages/LoginErrorPage';
 import LoginSuccessPage from './pages/LoginSuccessPage';
+import LandingPage from './pages/LandingPage';
 
 function App() {
   return (
     <MuiContainer>
-      <Header />
       <Switch>
-        <Route exact path="/">
-          <Button
-            href="http://localhost:5000/auth/google"
-            variant="contained"
-          >
-            Sign in with Goggles 😎
-          </Button>
-        </Route>
+        <Route exact path="/" component={LandingPage} />
         <Route exact path="/dashboard" component={DashBoardPage} />
         <Route exact path="/login/success" component={LoginSuccessPage} />
         <Route exact path="/login/error" component={LoginErrorPage} />
