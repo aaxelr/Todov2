b90894b35f3cf76b56239f33bc23f06dc4fe4983
aaxelr <antonio.axelsson@gmail.com>
2021-11-06 13:01:54 +0100

    pass edit function

--- client/src/components/Todos.jsx
+++ client/src/components/Todos.jsx
@@ -3,14 +3,20 @@ import PropTypes from 'prop-types';
 import Todo from './Todo';
 import MuiContainer from './UI/MuiContainer';
 
-function Todos({ todos, onDeleteTodo }) {
+function Todos({ todos, onDeleteTodo, onEditTodo }) {
   return (
     <MuiContainer>
       {!todos
       && <p>loading...</p>}
-      {todos && console.log(todos)}
       {todos.length > 0
-      && todos.map((todo) => <Todo todo={todo} key={todo.uuid} onDeleteTodo={onDeleteTodo} />)}
+      && todos.map((todo) => (
+        <Todo
+          todo={todo}
+          key={todo.uuid}
+          onDeleteTodo={onDeleteTodo}
+          onEditTodo={onEditTodo}
+        />
+      ))}
     </MuiContainer>
   );
 }
@@ -18,6 +24,7 @@ function Todos({ todos, onDeleteTodo }) {
 Todos.propTypes = {
   todos: PropTypes.arrayOf(PropTypes.object).isRequired,
   onDeleteTodo: PropTypes.func.isRequired,
+  onEditTodo: PropTypes.func.isRequired,
 };
 
 export default Todos;
