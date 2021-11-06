ed0c5a43ed4cac301a9639801037ddc056c3dc9e
aaxelr <antonio.axelsson@gmail.com>
2021-11-06 14:58:45 +0100

    update session cookie

--- server/app.js
+++ server/app.js
@@ -8,7 +8,6 @@ require('./auth/passportGoogle');
 const { isAuthenticated } = require('./middlewares/isAuthenticated');
 
 const authRouter = require('./routes/authRouter');
-const indexRouter = require('./routes/indexRouter');
 const todoRouter = require('./routes/todoRouter');
 
 const app = express();
@@ -17,26 +16,30 @@ const corsOrigin = process.env.CLIENT_URL || 'http://localhost:3000';
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
 app.use(cors({
   origin: corsOrigin,
   credentials: true,
 }));
-// NOTE set cookie to secure & httpOnly before deploying?
+
 app.use(session({
   secret: process.env.SESSION_SECRET,
   store: MongoStore.create({
     mongoUrl: process.env.DB_URI,
   }),
   resave: false,
-  saveUninitialized: true,
+  saveUninitialized: false,
   cookie: {
     maxAge: 1000 * 60 * 60 * 24 * 2,
+    httpOnly: true,
+    secure: true,
+    sameSite: 'none',
   },
 }));
+
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.use('/', indexRouter);
 app.use('/auth', authRouter);
 app.use('/api/v1/todos', isAuthenticated, todoRouter);
 
