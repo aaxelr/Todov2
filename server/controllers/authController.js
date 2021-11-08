4009ef1c470f30010dd7b5a1ad11a3cb89a1e175
aaxelr <antonio.axelsson@gmail.com>
2021-11-08 09:45:57 +0100

    refactor redirect urls

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,8 +1,8 @@
 const passport = require('passport');
 
-const CLIENT = process.env.CLIENT_URL;
-const successRedirect = `${CLIENT}/login/success` || 'http://localhost:3000/login/success';
-const failureRedirect = `${CLIENT}/login/error` || 'http://localhost:3000/login/error';
+const CLIENT = process.env.CLIENT_URL || 'http://localhost:3000';
+const successRedirect = `${CLIENT}/login/success`;
+const failureRedirect = `${CLIENT}/login/error`;
 
 exports.authUser = passport.authenticate('google', { scope: ['profile', 'email'] });
 
