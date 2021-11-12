83e82f5d5de991f0d887ca4c5d9a38c823d422db
aaxelr <antonio.axelsson@gmail.com>
2021-11-12 12:04:53 +0100

    remove redux-logger

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -1,10 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
-import logger from 'redux-logger';
 import authReducer from './auth';
 
 export default configureStore({
   reducer: {
     auth: authReducer,
   },
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
